6e32819fe6dbf86de4735ec5cda1d25057a9cac5
Pe-ngo <jamespengo95@gmail.com>
2022-06-19 12:32:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+// a function to check if the string is a Palindrome or not
+function isPalindrome(word)
+{
+  let j = word.length -1;
+  for( let i = 0 ; i < j/2 ;i++)
+  {
+    let x = word[i] ;//forward character
+    let y = word[j-i];//backward character
+    if( x !== y)
+    {
+      // return false if string not match
+      return false;
+    }
+  }
+  /// return true if string is palindrome
+  return true;
+   
 }
+
+//function that print output is string is palindrome
+function checkPalindrome(word)
+{
+  // variable that is true ig string is palindrome
+  let ans = isPalindrome(word);
+  //condition checking ans is true or not
+  if( ans == true )
+  {
+    console.log("passed string is palindrome ");
+  }
+  else
+  {
+    console.log("passed string not a palindrome");
+  }
+}
+// test variable
+let test = "racecar";
+checkPalindrome(test);
 
 /* 
   Add your pseudocode here
+  check if input (word is a palindrome )
+  if the word is a palindrome return true
+  if not return false 
 */
 
 /*
